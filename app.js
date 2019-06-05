0f61f6fdd927fb83ecf6b8e11bd6f4491239945e
binieth <abigiyabiniyam@gmail.com>
2019-06-05 00:19:30 -0400

    made additional change on the lay out. most specifically the footer section

--- app.js
+++ app.js
@@ -10,3 +10,35 @@ $('#view-work').on('click', function(){
         900
     );
 });
+
+// typing effect
+
+const texts = ['family & children photo', 'wedding & engagement', 'maternity photo', 'birthday photo', 'graduation photo' ];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+if(count === texts.length ){
+    count = 0;
+}
+currentText = texts[count];
+letter = currentText.slice(0, ++index);
+document.querySelector('.typing').textContent = letter;
+if(letter.length === currentText.length){
+    count++;
+    index = 0;
+}
+setTimeout(type, 400);
+
+}());
+
+// footer copyright year
+
+var today = new Date();
+var year  = today.getFullYear();
+
+var e = document.getElementById('copyright');
+e.innerHTML = '<p> Copyright &copy;' + year + '</p>';
